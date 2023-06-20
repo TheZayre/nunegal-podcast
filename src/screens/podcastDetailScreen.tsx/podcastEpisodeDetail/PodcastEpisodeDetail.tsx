7a3477cca7c1a8aa43ca28623980fda28240783f
Zayre <thezayrex@gmail.com>
2023-06-21 01:06:46 +0200

    Ultima pantalla terminada (detalle episodio)

--- src/screens/podcastDetailScreen.tsx/podcastEpisodeDetail/PodcastEpisodeDetail.tsx
+++ src/screens/podcastDetailScreen.tsx/podcastEpisodeDetail/PodcastEpisodeDetail.tsx
@@ -1,18 +1,28 @@
+import { useEffect, useState } from 'react';
 import './podcastepisodedetail.scss'
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { podcastInformationPodcastEpisodes, podcastInformationPodcastSelected } from 'redux/slices/podcastSlice';
+import { useNavigate, useParams } from 'react-router-dom';
+import { podcastInformationPodcastEpisodes, podcastInformationPodcastSelected, setDescription, setEpisodes, setPodcast } from 'redux/slices/podcastSlice';
 
 export default function PodcastEpisodeDetail() {
 
   const podcastEpisodes = useSelector(podcastInformationPodcastEpisodes)
-  const podcastSelected = useSelector(podcastInformationPodcastSelected)
-  const navigate = useNavigate();
+  const [episodeSelected, setEpisodeSelected] = useState() as any
+  const { id, idEpisode } = useParams();
 
+  useEffect(()=>{
+    let filter = podcastEpisodes.find(({guid})=>guid.split('/').join()===idEpisode)
+    setEpisodeSelected(filter)
+  },[podcastEpisodes])
   
   return (
     <div className={'podcast-episode-detail-content'}>
-      <div className={'title'}>{'Title'}</div>
+      <div className={'title'}>{episodeSelected?.title}</div>
+      {episodeSelected
+        ?<div className={'description'} dangerouslySetInnerHTML={{__html:episodeSelected["content:encoded"] ?? episodeSelected?.content}}/>
+        :null
+      }
+      <audio className={'audio'} controls src={episodeSelected?.enclosure?.url}></audio>
     </div>
   );
 }
--- src/screens/podcastDetailScreen.tsx/podcastEpisodes/PodcastEpisodes.tsx
+++ src/screens/podcastDetailScreen.tsx/podcastEpisodes/PodcastEpisodes.tsx
@@ -12,13 +12,17 @@ export default function PodcastEpisodes() {
   const renderEpisodes = () => {
     let res = [] as any;
     let i = 0;
+    
     for(let element in podcastEpisodes){
       let date = new Date(podcastEpisodes[element]?.isoDate) as any
       date = date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()
+
+      console.log(podcastEpisodes[element]?.guid);
+      console.log(podcastEpisodes[element]?.guid.split('/').join());
       
       res.push(
         <div className={'header'} key={element}>
-          <div className={i%2==0 ? 'title-element pair' : 'title-element'} onClick={()=>{navigate(`/podcast/${podcastSelected?.collectionId}/episode/${podcastEpisodes[element]?.guid}`);}}>
+          <div className={i%2==0 ? 'title-element pair' : 'title-element'} onClick={()=>{navigate(`/podcast/${podcastSelected?.collectionId}/episode/${podcastEpisodes[element]?.guid.split('/').join()}`);}}>
             {podcastEpisodes[element]?.title}
             </div>
           <div className={i%2==0 ? 'date-element pair' : 'date-element'}>{date}</div>
